46be99ecf40fd30849475419c8cec79404336c84
Mcdonalds Trump <whdmschd2@gmail.com>
2022-03-17 17:24:53 +0900

    Refactor: 코드정리

--- src/styles/global-style.ts
+++ src/styles/global-style.ts
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import { normalize } from 'styled-normalize';
 
-// 위에서 받은 `normalize`로 기본 css가 초기화 합니다.
 const GlobalStyle = createGlobalStyle`
   ${normalize}
 
