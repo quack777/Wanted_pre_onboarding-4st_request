aea3abf89e61779c5909ff11cb6740e64173aa1e
Mcdonalds Trump <whdmschd2@gmail.com>
2022-03-17 22:06:36 +0900

    Add: font

--- src/styles/global-style.ts
+++ src/styles/global-style.ts
@@ -2,10 +2,12 @@ import { createGlobalStyle } from 'styled-components';
 import { normalize } from 'styled-normalize';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
   ${normalize}
 
   * {
     box-sizing: border-box;
+    font-family: 'Noto Sans KR', sans-serif;
   }
   ul {
     list-style:none;
