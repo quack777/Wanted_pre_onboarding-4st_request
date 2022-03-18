d82c8aeb0a43c632468f61f3664d050e0e2ac904
Mcdonalds Trump <whdmschd2@gmail.com>
2022-03-18 22:12:30 +0900

    Refactor: 코드 정리

--- src/hooks/useInfoListState.ts
+++ src/hooks/useInfoListState.ts
@@ -32,7 +32,6 @@ const useInfoListState = ({ method, material, status }: OptionalProps) => {
       }
       return false;
     }
-    // return JSON.stringify(listMethod) === JSON.stringify(method);
     if (method.filter((x) => listMethod.includes(x)).length > 0) {
       return true;
     }
@@ -53,7 +52,6 @@ const useInfoListState = ({ method, material, status }: OptionalProps) => {
       }
       return false;
     }
-    // return JSON.stringify(listMaterial) === JSON.stringify(material);
     if (material.filter((x) => listMaterial.includes(x)).length > 0) {
       return true;
     }
@@ -93,8 +91,6 @@ const useInfoListState = ({ method, material, status }: OptionalProps) => {
   const checkStatusInfoList = () => {
     if (status) {
       if (method.length > 0 || material.length > 0) {
-        console.log('필터선택되어있음');
-        console.log(sortedInfoList);
         setSortedInfoList(sortedInfoList.filter((list) => list.status === '상담중'));
       } else {
         setSortedInfoList(infoList.filter((list) => list.status === '상담중'));
@@ -118,95 +114,6 @@ const useInfoListState = ({ method, material, status }: OptionalProps) => {
   useEffect(() => {
     checkStatusInfoList();
   }, [status]);
-
-  /* const checkSortedInfoList = (
-    currentSort: string[],
-    prevSort: string[],
-    currentSortName: string,
-    prevSortName: string,
-  ): void => {
-    const currentSelectedSortNum: number = currentSort.length;
-    const prevSelectedSortNum: number = prevSort.length;
-
-    if (currentSelectedSortNum !== 0) {
-      let filteredInfoList = sortedInfoList.slice();
-
-      infoList.forEach((info: InfoType) => {
-        let isSorted = false;
-        for (let i = 0; i < currentSelectedSortNum; i++) {
-          if (info[currentSortName].includes(currentSort[i])) {
-            isSorted = true;
-            break;
-          }
-        }
-
-        const currentStoredInfoID = filteredInfoList.map((info) => {
-          return info.id;
-        });
-
-        if (isSorted && !currentStoredInfoID.includes(info.id)) {
-          filteredInfoList.push(info);
-        }
-
-        if (!isSorted && currentStoredInfoID.includes(info.id)) {
-          filteredInfoList = filteredInfoList.filter((storedInfo) => storedInfo.id !== info.id);
-        }
-      });
-
-      setSortedInfoList(filteredInfoList);
-    } else if (prevSelectedSortNum !== 0) {
-      const filteredInfoList = sortedInfoList.slice();
-
-      infoList.forEach((info: InfoType) => {
-        let isSorted = false;
-        for (let i = 0; i < prevSelectedSortNum; i++) {
-          if (info[prevSortName].includes(prevSort[i])) isSorted = true;
-          else {
-            isSorted = false;
-            break;
-          }
-        }
-
-        const currentStoredInfoID = filteredInfoList.map((info) => {
-          return info.id;
-        });
-
-        if (isSorted && !currentStoredInfoID.includes(info.id)) {
-          filteredInfoList.push(info);
-        }
-      });
-
-      setSortedInfoList(filteredInfoList);
-    } else setSortedInfoList([]);
-  };
-
-  useEffect(() => {
-    checkSortedInfoList(method, material, 'method', 'material');
-  }, [method]);
-
-  useEffect(() => {
-    checkSortedInfoList(material, method, 'material', 'method');
-  }, [material]);
-
-  useEffect(() => {
-    if (status) {
-      const currentInfoList = sortedInfoList.length === 0 ? infoList : sortedInfoList;
-
-      const filteredInfoList: InfoType[] = currentInfoList.filter((info: InfoType) => info.status === '상담중');
-      setSortedInfoList(filteredInfoList);
-    } else {
-      checkSortedInfoList(method, material, 'method', 'material');
-      checkSortedInfoList(material, method, 'material', 'method');
-    }
-  }, [status]);
-
-  useEffect(() => {
-    if (!isEmpty) return;
-    const currentInfoListNum = sortedInfoList.length;
-
-    if (currentInfoListNum === 0) setIsEmpty(true);
-    else setIsEmpty(false);
-  }, [sortedInfoList]); */
 
   return { infoList, sortedInfoList, isEmpty };
 };
