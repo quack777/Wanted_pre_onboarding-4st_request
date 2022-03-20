0a90284aa46592aaa321dd0afb528f229b92bd6d
Mcdonalds Trump <whdmschd2@gmail.com>
2022-03-20 22:23:45 +0900

    Refactor: toggle

--- src/components/setFilter/Toggle.tsx
+++ src/components/setFilter/Toggle.tsx
@@ -7,7 +7,7 @@ import { isToggleOn } from '../../modules/filter/actions';
 const Toggle = () => {
   const dispatch = useDispatch();
   const isToggleHanddle = useCallback(() => dispatch(isToggleOn()), [dispatch]);
-  const status = useSelector(({ toggle }: RootState) => toggle.isConsulting);
+  const isConsulting = useSelector(({ toggle }: RootState) => toggle.isConsulting);
 
   const handleClickToggle = () => {
     isToggleHanddle();
@@ -15,16 +15,16 @@ const Toggle = () => {
 
   return (
     <ToggleBox>
-      {status ? (
-        <button type="submit" onClick={handleClickToggle}>
-          <img alt="toggleOn" src="Image/toggle_on.png" />
-        </button>
+      {isConsulting ? (
+        <ToggleBtn type="submit" onClick={handleClickToggle}>
+          <img alt="toggle_on" src="Image/toggle_on.png" />
+        </ToggleBtn>
       ) : (
-        <button type="submit" onClick={handleClickToggle}>
-          <img alt="toggleOff" src="Image/toggle_off.png" />
-        </button>
+        <ToggleBtn type="submit" onClick={handleClickToggle}>
+          <img alt="toggle_off" src="Image/toggle_off.png" />
+        </ToggleBtn>
       )}
-      <p>상담 중인 요청만 보기</p>
+      <ToggleInfo>상담 중인 요청만 보기</ToggleInfo>
     </ToggleBox>
   );
 };
@@ -38,21 +38,24 @@ const ToggleBox = styled.div`
   @media (max-width: 600px) {
     margin-top: 20px;
   }
-  & > button {
-    border: none;
-    background: #ffffff;
-    width: 37px;
-    height: 20px;
-    padding: 0;
-    cursor: pointer;
-  }
-  & > p {
-    position: relative;
-    top: 1px;
-    font-family: Noto Sans KR Medium;
-    font-size: 14px;
-    color: #323d45;
-    margin: 0;
-  }
 `;
+
+const ToggleBtn = styled.button`
+  border: none;
+  background: #ffffff;
+  width: 37px;
+  height: 20px;
+  padding: 0;
+  cursor: pointer;
+`;
+
+const ToggleInfo = styled.p`
+  position: relative;
+  top: 1px;
+  font-family: Noto Sans KR Medium;
+  font-size: 14px;
+  color: #323d45;
+  margin: 0;
+`;
+
 export default Toggle;
